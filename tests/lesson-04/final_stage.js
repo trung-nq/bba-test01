1153fb85f597043f687d7b643c771caa8c303caf
test 01 - Nguyen Quoc Trung <trungnq16091997@gmail.com>
2026-04-01 00:07:35 +0700

    Another way for final stage

--- tests/lesson-04/final_stage.js
+++ tests/lesson-04/final_stage.js
@@ -12,12 +12,23 @@ let selectedNumbersSet = new Set();
   
 
 //  Anws2:
-for (let i = 1; i <= 100; i++) {
-    for (let j = i; j <= 100; j ++ ){
-        if((i + j) % 17 === 0){
-            let pair = i > j ? [i, j] : [j, i];
-            selectedNumbersSet.add(pair.toString());
-        }
-    }
+// for (let i = 1; i <= 100; i++) {
+//     for (let j = i; j <= 100; j ++ ){
+//         if((i + j) % 17 === 0){
+//             let pair = i > j ? [i, j] : [j, i];
+//             selectedNumbersSet.add(pair.toString());
+//         }
+//     }
+// }
+// console.log(selectedNumbersSet.size);
+
+// Anws3:
+
+for (let i =1; i <= 100; i++){
+    let targetNumber = 17 - (i % 17);
+    for (let j = targetNumber; j <= 100; j +=17){
+        let pair = i > j ?  [i,j] : [j,i] ;
+        selectedNumbersSet.add(pair.toString());
+    } 
 }
 console.log(selectedNumbersSet.size);
